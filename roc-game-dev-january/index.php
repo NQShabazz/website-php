60b16f4597b99e278a4f2104151b41ab7e8a61e8
NQShabazz <nqshabazz@gmail.com>
2018-01-06 22:20:21 -0500

    added a new blog post

---
+++ roc-game-dev-january/index.php
@@ -0,0 +1,110 @@
+<?php
+// Personal Home Page or PHP: Hypertext Processor
+include '../defaults.inc.php';
+$to_root=get_rel_path(__DIR__, 'C:/Users/Nazaire/Desktop/My Projects/website/website-php');
+
+$title='ROC Game Dev January';
+$dashed_title=str_replace(' ', '-', strtolower($title));
+$image_rpath="../assets/images/why-2018-will-be-lit/top-image.png";
+$tags="roc-game-dev gamedev .january .2018";
+$excerpt="So on Wednesday (1/3/2018) I went to the ROC Game Dev Meetup.";
+$entry_date = date('c');
+
+$trimmed_excerpt=$excerpt;
+
+if(strlen($trimmed_excerpt) > 255);
+  $trimmed_excerpt=substr($trimmed_excerpt, 0, 252).'...';
+
+$page_title=$title.' - Nazaire Shabazz';
+$page_description=$trimmed_excerpt;
+$page_image="/".$image_rpath;
+$page_url="/".$dashed_title;
+
+$trimmed_excerpt = addslashes($trimmed_excerpt);
+
+//RESOLVE DIR PROBLEM
+$blog_path="C:/Users/Nazaire/Desktop/My Projects/website/nqshabazz.github.io/".$dashed_title;
+start_doc($blog_path);
+?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydatabase";
+$tablename = "myblogs";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error);
+}
+
+$sql = "SELECT reg_date FROM myblogs WHERE title='".$title."'";
+$result0 = $conn->query($sql);
+
+if($result0->num_rows>0){
+  $row = ($result0->fetch_assoc());
+  $entry_date = $row["reg_date"];
+}
+?>
+  <main id="post-page">
+    <article id='blog-post' class='m-auto'>
+      <h1 class="default-box" id="front-page-introduction"><?php echo $title ?></h1>
+      <p class="text-muted">
+        by Nazaire Shabazz | <time datetime="<?php echo date("c", strtotime($entry_date)) ?>" title="<?php echo date('n/d/y h:i:s a', strtotime($entry_date)) ?>"><?php echo date('n/d/y h:i:s a', strtotime($entry_date)) ?></time>
+      </p>
+      <p id="post-tags">
+        <?php foreach(explode(' ', $tags) as $tag) echo "<a class='badge badge-default' href='../#".$tag."'>".$tag."</a>" ?>
+      </p>
+      <hr class="default-box" id="front-page-loading-bar" />
+      <img src="<?php echo $image_rpath ?>"/>
+      <div id="content-container" class="bg-faded">
+        <p><?php echo $excerpt ?></p>
+        
+        <p>It's really cool! The first Wednesday of every month, game developers of Rochester will get together to chat and present their projects.</p>
+        
+        <p>The first project presented was a 2D top-down horror game. The player is lost in a crypt of some sort, and must rely on an oil lamp to find their way out. The oil lamp flickers out over time, so the player has to find oil and be quick to progress. So far the creator has lighting and sprites set up, and based on some of the concept art, there will be undead creatures roaming the levels.</p>
+        
+        <p>The second project was a remake of Halloween Forever, a halloween-themed platformer. But the amazing part was the hardware they created: an arcade controller with a joystick and two-button input. It was apparently super cheap and easy for them too.</p>
+        
+        <p>The third game was Project Hunt by Craft Brewed Games. The game takes place on a hex-tile grid with three points of interest: the animal, it's destination, and the hunter. Each turn, the animal goes one tile closer to it's destination, and the hunter attempts to intercept it. As the player, you can make one tile untraversable each turn (up to four blocked tiles total). Project Hunt is simple, challenging, and relaxing.</p>
+        
+        <p>The last thing I saw before I had to go was ChimeN. It's a social app that takes local news and events and lets the user quickly chime in by reacting with an emote. I can already see how this could be useful as a quick polling app or a local invitation app. It also has a little "room" system to join even more localized chats. So if you're in a coffee shop for example, you can join the "coffee room" and chat with others in the room.</p>
+        <hr/>
+      </div>
+      <?php include '../includes/post-ending.php' ?>
+      <script>
+        
+      </script>
+    </article>
+  </main>
+<?php
+//Personal Home Page
+
+//title, reg_date, image_rpath, tags, excerpt
+
+$sql = "SELECT * FROM myblogs WHERE title='".$title."'";
+$result = $conn->query($sql);
+
+if($result->num_rows<=0){
+  $sql0 = "INSERT INTO myblogs (title, image_rpath, tags, excerpt) VALUES ('".$title."', '".$image_rpath."', '".$tags."', '".$trimmed_excerpt."')";
+  
+  if($conn->query($sql0)!==TRUE){
+      echo "Error: ".$sql0."<br>".$conn->error;
+  }
+}else{
+  $sql0 = "UPDATE myblogs SET title='".$title."', image_rpath='".$image_rpath."', tags='".$tags."', excerpt='".$trimmed_excerpt."' WHERE title='".$title."'";
+  
+  if($conn->query($sql0)!==TRUE){
+      echo "Error: ".$sql0."<br>".$conn->error;
+  }
+}
+
+$conn->close();
+?>
+<?php
+// Personal Home Page or PHP: Hypertext Processor
+end_doc();
+?>
